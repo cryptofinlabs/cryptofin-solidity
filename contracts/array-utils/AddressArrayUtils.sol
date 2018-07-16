87ebe7a7bfee7619149e9eb538c51dfd34019fb8
Daniel Que <dmdque@gmail.com>
2018-07-16 16:14:06 -0700

    Add isEqual and remove by value, fix style

--- contracts/array-utils/AddressArrayUtils.sol
+++ contracts/array-utils/AddressArrayUtils.sol
@@ -3,84 +3,111 @@ pragma solidity 0.4.24;
 
 library AddressArrayUtils {
 
-    /**
-     * @return Returns index and isIn for the first occurrence starting from index 0
-     */ 
-    function indexOf(address[] addresses, address a) internal pure returns (uint256, bool) {
-        for (uint256 i = 0; i < addresses.length; i++) {
-            if (addresses[i] == a) {
-                return (i, true);
-            }
-        }
-        return (0, false);
+  /**
+   * @return Returns index and isIn for the first occurrence starting from index 0
+   */ 
+  function indexOf(address[] A, address a) internal pure returns (uint256, bool) {
+    uint256 length = A.length;
+    for (uint256 i = 0; i < length; i++) {
+      if (A[i] == a) {
+        return (i, true);
+      }
     }
+    return (0, false);
+  }
 
-    /**
-     * @return Returns isIn for the first occurrence starting from index 0
-     */ 
-    function contains(address[] addresses, address a) internal pure returns (bool) {
-      bool isIn;
-      (, isIn) = indexOf(addresses, a);
-      return isIn;
+  /**
+  * @return Returns isIn for the first occurrence starting from index 0
+  */ 
+  function contains(address[] A, address a) internal pure returns (bool) {
+    (, bool isIn) = indexOf(A, a);
+    return isIn;
+  }
+
+
+  /// @return Returns index and isIn for the first occurrence starting from
+  /// end
+  function indexOfFromEnd(address[] A, address a) internal pure returns (uint256, bool) {
+    uint256 length = A.length;
+    for (uint256 i = length; i > 0; i--) {
+      if (A[i - 1] == a) {
+        return (i, true);
+      }
     }
+    return (0, false);
+  }
 
+  function extend(address[] storage A, address[] storage B) internal {
+    uint256 length = B.length;
+    for (uint256 i = 0; i < length; i++) {
+      A.push(B[i]);
+    }
+  }
 
-    /// @return Returns index and isIn for the first occurrence starting from
-    /// end
-    function indexOfFromEnd(address[] addresses, address a) internal pure returns (uint256, bool) {
-      for (uint256 i = addresses.length; i > 0; i--) {
-        if (addresses[i - 1] == a) {
-          return (i, true);
+  /**
+  * @dev Reverses address array in place
+  */
+  function reverse(address[] storage A) internal {
+    address t;
+    uint256 length = A.length;
+    for (uint256 i = 0; i < length / 2; i++) {
+      t = A[i];
+      A[i] = A[A.length - i - 1];
+      A[A.length - i - 1] = t;
+    }
+  }
+
+  /**
+  * Deletes address at index and fills the spot with the last address
+  * @return Returns the new array length
+  */
+  function removeIndex(address[] storage A, uint256 index) internal returns (uint256) {
+    if (index >= A.length) {
+      return A.length;
+    }
+    A[index] = A[A.length - 1];
+    delete A[A.length - 1];
+    A.length--;
+    return A.length;
+  }
+
+  /**
+   * Deletes address at index and fills the spot with the last address
+   * @return Returns the new array length
+   */
+  function remove(address[] storage A, address a) internal returns (uint256) {
+    (uint256 index, bool isIn) = indexOf(A, a);
+    if (!isIn) {
+      return A.length;
+    } else {
+      return removeIndex(A, index);
+    }
+  }
+
+  /**
+   * Returns whether or not there's a duplicate
+   * Runs in O(n^2)
+   */
+  function hasDuplicate(address[] A) internal pure returns (bool) {
+    uint256 length = A.length;
+    for (uint256 i = 0; i < length - 1; i++) {
+      for (uint256 j = i + 1; j < length; j++) {
+        if (A[i] == A[j]) {
+          return true;
         }
       }
-      return (0, false);
     }
+    return false;
+  }
 
-    function extend(address[] storage a, address[] storage b) internal {
-        for (uint256 i = 0; i < b.length; i++) {
-            a.push(b[i]);
-        }
-    }
-
-    /**
-     * @dev Reverses address array in place
-     */
-    function reverse(address[] storage a) internal {
-        address t;
-        for (uint256 i = 0; i < a.length / 2; i++) {
-            t = a[i];
-            a[i] = a[a.length - i - 1];
-            a[a.length - i - 1] = t;
-        }
-    }
-
-    /**
-     * Deletes address at index and fills the spot with the last address
-     * @return Returns the new array length
-     */
-    function remove(address[] storage addresses, uint256 index) internal returns (uint256) {
-      if (index >= addresses.length) {
-        return addresses.length;
+  function isEqual(address[] A, address[] B) internal pure returns (bool) {
+    uint256 length = A.length;
+    for (uint256 i = 0; i < length; i++) {
+      if (A[i] != B[i]) {
+        return false;
       }
-      addresses[index] = addresses[addresses.length - 1];
-      delete addresses[addresses.length - 1];
-      addresses.length--;
-      return addresses.length;
     }
-
-    /**
-     * Returns whether or not there's a duplicate
-     * Runs in O(n^2)
-     */
-    function hasDuplicate(address[] addresses) internal pure returns (bool) {
-      for (uint256 i = 0; i < addresses.length - 1; i++) {
-        for (uint256 j = i + 1; j < addresses.length; j++) {
-          if (addresses[i] == addresses[j]) {
-            return true;
-          }
-        }
-      }
-      return false;
-    }
+    return true;
+  }
 
 }
--- test/array-utils/TestAddressArrayUtils.sol
+++ test/array-utils/TestAddressArrayUtils.sol
@@ -8,78 +8,132 @@ import "../../contracts/array-utils/AddressArrayUtils.sol";
 
 
 contract TestAddressArrayUtils {
-    using AddressArrayUtils for address[];
+  using AddressArrayUtils for address[];
 
-    address[] addressesA;
-    address[] addressesB;
-    address[] addressesC;
+  address[] addressesA;
+  address[] addressesB;
+  address[] addressesC;
 
-    function beforeEach() public {
-        addressesA.length = 0;
-        addressesB.length = 0;
-        addressesC.length = 0;
+  function beforeEach() public {
+    addressesA.length = 0;
+    addressesB.length = 0;
+    addressesC.length = 0;
 
-        addressesA.push(address(0x1));
-        addressesA.push(address(0x2));
-        addressesA.push(address(0x3));
-        addressesA.push(address(0x4));
+    addressesA.push(address(0x1));
+    addressesA.push(address(0x2));
+    addressesA.push(address(0x3));
+    addressesA.push(address(0x4));
 
-        addressesB.push(address(0x8));
-        addressesB.push(address(0x9));
-        addressesB.push(address(0x10));
-    }
+    addressesB.push(address(0x8));
+    addressesB.push(address(0x9));
+    addressesB.push(address(0x10));
+  }
 
-    function testIndexFindsItem() public {
-        uint256 index;
-        bool isIn;
-        (index, isIn) = addressesA.indexOf(address(0x2));
-        Assert.isTrue(isIn, "should be ok");
-        Assert.equal(index, 1, "should return index 1");
-    }
+  function testIsEqualTrue() public {
+    addressesC.push(address(0x1));
+    addressesC.push(address(0x2));
+    addressesC.push(address(0x3));
+    addressesC.push(address(0x4));
+    bool equal = addressesA.isEqual(addressesC);
+    Assert.equal(equal, true, "should return equal");
+  }
 
-    function testIndexDoesNotFindItem() public {
-        uint256 index;
-        bool isIn;
-        (index, isIn) = addressesA.indexOf(address(0x0));
-        Assert.isFalse(isIn, "should not be ok");
-        Assert.equal(index, 0, "should return index 0");
-    }
+  function testIsEqualFalse() public {
+    bool equal = addressesA.isEqual(addressesB);
+    Assert.equal(equal, false, "should not return not equal");
+  }
 
-    function testContainsFindsItem() public {
-        bool isIn = addressesA.contains(0x3);
-        Assert.isTrue(isIn, "should be true");
-    }
+  function testIndexFindsItem() public {
+    (uint256 index, bool isIn) = addressesA.indexOf(address(0x2));
+    Assert.isTrue(isIn, "should be ok");
+    Assert.equal(index, 1, "should return index 1");
+  }
 
-    function testContainsDoesNotFindItem() public {
-        bool isIn = addressesA.contains(0x12);
-        Assert.isFalse(isIn, "should be false");
-    }
+  function testIndexDoesNotFindItem() public {
+    (uint256 index, bool isIn) = addressesA.indexOf(address(0x0));
+    Assert.isFalse(isIn, "should not be ok");
+    Assert.equal(index, 0, "should return index 0");
+  }
 
-    function testExtendExtends() public {
-        addressesA.extend(addressesB);
-        Assert.equal(addressesA.length, 7, "extended length should be 7");
-    }
+  function testContainsFindsItem() public {
+    bool isIn = addressesA.contains(0x3);
+    Assert.isTrue(isIn, "should be true");
+  }
 
-    function testExtendExtendsEmpty() public {
-        addressesA.extend(addressesC);
-        Assert.equal(addressesA.length, 4, "extended length should be 4");
-    }
+  function testContainsDoesNotFindItem() public {
+    bool isIn = addressesA.contains(0x12);
+    Assert.isFalse(isIn, "should be false");
+  }
 
-    function testReverseEven() public {
-        addressesA.reverse();
-        Assert.equal(addressesA.length, 4, "reversed length should be 4");
-        Assert.equal(addressesA[0], address(0x4), "element 0 should match");
-        Assert.equal(addressesA[1], address(0x3), "element 1 should match");
-        Assert.equal(addressesA[2], address(0x2), "element 2 should match");
-        Assert.equal(addressesA[3], address(0x1), "element 3 should match");
-    }
+  function testExtendExtends() public {
+    addressesA.extend(addressesB);
+    Assert.equal(addressesA.length, 7, "extended length should be 7");
+  }
 
-    function testReverseOdd() public {
-        addressesB.reverse();
-        Assert.equal(addressesB.length, 3, "reversed length should be 3");
-        Assert.equal(addressesB[0], address(0x10), "element 0 should match");
-        Assert.equal(addressesB[1], address(0x9), "element 1 should match");
-        Assert.equal(addressesB[2], address(0x8), "element 2 should match");
-    }
+  function testExtendExtendsEmpty() public {
+    addressesA.extend(addressesC);
+    Assert.equal(addressesA.length, 4, "extended length should be 4");
+  }
+
+  function testReverseEven() public {
+    addressesA.reverse();
+    Assert.equal(addressesA.length, 4, "reversed length should be 4");
+    Assert.equal(addressesA[0], address(0x4), "element 0 should match");
+    Assert.equal(addressesA[1], address(0x3), "element 1 should match");
+    Assert.equal(addressesA[2], address(0x2), "element 2 should match");
+    Assert.equal(addressesA[3], address(0x1), "element 3 should match");
+  }
+
+  function testReverseOdd() public {
+    addressesB.reverse();
+    Assert.equal(addressesB.length, 3, "reversed length should be 3");
+    Assert.equal(addressesB[0], address(0x10), "element 0 should match");
+    Assert.equal(addressesB[1], address(0x9), "element 1 should match");
+    Assert.equal(addressesB[2], address(0x8), "element 2 should match");
+  }
+
+  function testRemoveIndex() public {
+    uint256 length = addressesA.removeIndex(2);
+    Assert.equal(length, 3, "returned length should be 3");
+    Assert.equal(addressesA.length, 3, "removed length should be 3");
+    Assert.equal(addressesA[0], address(0x1), "element 0 should match");
+    Assert.equal(addressesA[1], address(0x2), "element 1 should match");
+    Assert.equal(addressesA[2], address(0x4), "element 2 should match");
+  }
+
+  function testRemoveIndexOutOfBounds() public {
+    uint256 length = addressesA.removeIndex(7);
+    Assert.equal(length, 4, "returned length should be 4");
+    Assert.equal(addressesA.length, 4, "length should be 4");
+    Assert.equal(addressesA[0], address(0x1), "element 0 should match");
+    Assert.equal(addressesA[1], address(0x2), "element 1 should match");
+    Assert.equal(addressesA[2], address(0x3), "element 2 should match");
+    Assert.equal(addressesA[3], address(0x4), "element 3 should match");
+  }
+
+  function testRemoveIndexLength0() public {
+    uint256 length = addressesC.removeIndex(0);
+    Assert.equal(length, 0, "returned length should be 0");
+    Assert.equal(addressesC.length, 0, "length should be 3");
+  }
+
+  function testRemove() public {
+    uint256 length = addressesA.remove(0x1);
+    Assert.equal(length, 3, "returned length should be 3");
+    Assert.equal(addressesA.length, 3, "removed length should be 3");
+    Assert.equal(addressesA[0], address(0x4), "element 0 should match");
+    Assert.equal(addressesA[1], address(0x2), "element 1 should match");
+    Assert.equal(addressesA[2], address(0x3), "element 2 should match");
+  }
+
+  function testRemoveDoesNotExist() public {
+    uint256 length = addressesA.remove(0x111);
+    Assert.equal(length, 4, "returned length should be 4");
+    Assert.equal(addressesA.length, 4, "length should be 4");
+    Assert.equal(addressesA[0], address(0x1), "element 0 should match");
+    Assert.equal(addressesA[1], address(0x2), "element 1 should match");
+    Assert.equal(addressesA[2], address(0x3), "element 2 should match");
+    Assert.equal(addressesA[3], address(0x4), "element 3 should match");
+  }
 
 }
