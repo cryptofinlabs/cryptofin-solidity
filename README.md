0363e05bbf7229b77b37754b019784eeca852c48
Daniel Que <dmdque@gmail.com>
2018-07-20 16:46:56 -0700

    Add uint array and rational math functions

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # CryptoFin Solidity
 A collection of Solidity libraries.
+
+
+# To Do
+Generic map and reduce with bytes32[]?
+
+    function map(bytes32[] memory A, function (bytes32) pure returns (bytes32) fn);
+    function reduce(bytes32[] memory A, function (bytes32, bytes32) pure returns (bytes32) fn);
+
+Then just write functions that operate on them eg.
+
+    function doubleUInt256(bytes32) returns (bytes32);
+
+    function addUInt256(bytes32, bytes32) returns (bytes32);
+    function addUInt128(bytes32, bytes32) returns (bytes32);
+    function mulUInt256(bytes32, bytes32) returns (bytes32);
+    function divUInt256(bytes32, bytes32) returns (bytes32);
+
+    // Rational128
+    function computePercentageRational128(bytes32, bytes32) returns (bytes32);
+
--- contracts/array-utils/UIntArrayUtils.sol
+++ contracts/array-utils/UIntArrayUtils.sol
@@ -3,28 +3,52 @@ pragma solidity ^0.4.18;
 
 library UIntArrayUtils {
 
-    function map(uint[] memory a, function(uint) pure returns (uint) f)
-        internal
-        pure
-        returns (uint[] memory)
-    {
-        uint[] memory r = new uint[](a.length);
-        for (uint i = 0; i < a.length; i++) {
-            r[i] = f(a[i]);
-        }
-        return r;
+  function map(uint256[] memory A, function(uint256) pure returns (uint256) fn)
+    internal
+    pure
+    returns (uint256[] memory)
+  {
+    uint256[] memory mapped = new uint256[](A.length);
+    for (uint256 i = 0; i < A.length; i++) {
+      mapped[i] = fn(A[i]);
     }
+    return mapped;
+  }
 
-    function reduce(uint[] memory a, function(uint, uint) pure returns (uint) f)
-        internal
-        pure
-        returns (uint)
-    {
-        uint r = a[0];
-        for (uint i = 1; i < a.length; i++) {
-            r = f(r, a[i]);
-        }
-        return r;
+  function reduce(uint256[] memory A, function(uint256, uint256) pure returns (uint256) fn)
+    internal
+    pure
+    returns (uint256)
+  {
+    uint256 reduced = A[0];
+    for (uint256 i = 1; i < A.length; i++) {
+      reduced = fn(reduced, A[i]);
     }
+    return reduced;
+  }
+
+  /**
+   * @return Returns index and isIn for the first occurrence starting from index 0
+   */ 
+  function indexOf(uint256[] memory A, uint256 a) internal pure returns (uint256, bool) {
+    uint256 length = A.length;
+    for (uint256 i = 0; i < length; i++) {
+      if (A[i] == a) {
+        return (i, true);
+      }
+    }
+    return (0, false);
+  }
+
+  function sRemoveIndex(uint256[] storage A, uint256 index) internal returns (uint256) {
+    uint256 length = A.length;
+    if (index >= length) {
+      return length;
+    }
+    A[index] = A[length - 1];
+    delete A[length - 1];
+    A.length--;
+    return A.length;
+  }
 
 }
